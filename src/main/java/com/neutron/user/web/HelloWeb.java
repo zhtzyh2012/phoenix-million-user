4a6ea17fccb9bfd46022919b371e4acac19e354b
zhanghongtu <zhanghongtu@quandashi.com>
2019-05-07 12:12:54 +0800

    设置项目根路径,访问端口,测试前后端可以联调

---
+++ src/main/java/com/neutron/user/web/HelloWeb.java
@@ -0,0 +1,19 @@
+package com.neutron.user.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试当前系统的前后端口联调
+ * @author neutron
+ */
+@RestController
+public class HelloWeb {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
