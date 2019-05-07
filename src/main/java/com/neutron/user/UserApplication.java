02337391de03f173d120e97cb2927f1fd6b0f533
zhanghongtu <zhanghongtu@quandashi.com>
2019-05-07 12:19:44 +0800

    关于知识内容的整理

--- src/main/java/com/neutron/user/UserApplication.java
+++ src/main/java/com/neutron/user/UserApplication.java
@@ -21,13 +21,3 @@ public class UserApplication {
     }
 
 }
-
-/*
- * 知识介绍:
- * 1.其中@SpringBootApplication注解是spring boot的核心注解
- * 2.因为spring boot是jar包运行,所以必须有main方法
- * 3.问题:spring boot项目中虽然配置redis依赖,但是不想让spring boot自动检测redis配置,如何处理呢?
- *   答案1: @SpringBootApplication(exclude = {RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
- *   答案2: 删除redis依赖配置
- * 4.当增加mybatis依赖后,那么就需要配置数据库的配置文件,所以如果此时不需要数据库配置,注释掉mybatis依赖.
- */
